6b21a3f705d8abb0d9c646b9f8a58c3598946554
ShirZaguri <zagurishir@gmail.com>
2021-11-27 11:37:35 +0200

    fix(weather.service): axios error catching

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -9,7 +9,7 @@ export default class weatherService {
     const DAYS_FROM_TODAY = weatherService.daysFromToday(options.date);
     const DAY_WEATHER_URL = `${ACCU_WEATHER_BASE_URL}/${WEATHER_TYPE}/328328?day=${DAYS_FROM_TODAY}`;
 
-    await axios.get(
+    await fetch(
       "https://weather.com/he-IL/weather/today/l/ISXX0010:1:IS?Goto=Redirected"
     );
     console.log("3");
