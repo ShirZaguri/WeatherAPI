fe07ee3415fb73e4bfb9fc4d340e63a1d7c5902d
ShirZaguri <zagurishir@gmail.com>
2021-11-27 10:34:43 +0200

    heroku

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import logger from "@shared/Logger";
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   logger.info(`⚡️ [server]: Server is running at https://localhost:${PORT}`);
 });
