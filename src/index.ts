59831ed54705d15c1099db3c2e05249d55335067
ShirZaguri <zagurishir@gmail.com>
2021-11-27 10:47:45 +0200

    MERGE CONFLICTS

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,6 @@ import logger from "@shared/Logger";
 
 const PORT = 3000;
 
-<<<<<<< HEAD
 app.listen(process.env.PORT || PORT, () => {
   logger.info(`⚡️ [server]: Server is running at https://localhost:${PORT}`);
-=======
-app.listen(PORT, () => {
-  logger.info(`⚡️ [server]: Server is running at http://localhost:${PORT}`);
->>>>>>> 006be82ff92ed9bf7200f83cafeaa1701b14c479
 });
